7b2c0f560a4a50e3c0ea442d4136b3a890bdded1
Akanksha <akanksha.agarwal@inmobi.com>
2021-11-08 19:44:02 +0530

    inital commit

---
+++ HelloWorld/src/test/java/HelloWorldTest.java
@@ -0,0 +1,13 @@
+public class HelloWorldTest {
+    public void testHello() {
+        System.out.println("testing");
+    }
+
+    public void testWorld() {
+        System.out.println("world");
+    }
+
+    public void random() {
+        System.out.println("random");
+    }
+}
